da8fa0c2559a53a871452f40efc3a60fd36e1fe4
Matt Hough <matt@hough.tv>
2017-10-14 22:46:25 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,5 +10,6 @@ end
 # projects: kickstarter.css("li.project.grid_4")
 # title: kickstarter.css("h2.bbcard_name strong a").text
 # image: kickstarter.css("div.project-thumbnail a img").attribute("src").value
-# image: kickstarter.css("p.bbcard_blurb").text
+# description: kickstarter.css("p.bbcard_blurb").text
+# location: kickstarter.css("span.location-name").text
 create_project_hash
