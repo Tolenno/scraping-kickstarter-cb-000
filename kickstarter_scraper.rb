88f02d089d8d1714209304542dcaf04f09499c9a
Matt Hough <matt@hough.tv>
2017-10-14 22:55:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,14 +10,16 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {}
+
+    image = 
   end
   projects
 end
 
 # projects: kickstarter.css("li.project.grid_4")
 # title: kickstarter.css("h2.bbcard_name strong a").text
-# image: kickstarter.css("div.project-thumbnail a img").attribute("src").value
-# description: kickstarter.css("p.bbcard_blurb").text
-# location: kickstarter.css("span.location-name").text
-# percent_funded: kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+# image: projects.css("div.project-thumbnail a img").attribute("src").value
+# description: projects.css("p.bbcard_blurb").text
+# location: projects.css("span.location-name").text
+# percent_funded: projects.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 create_project_hash
