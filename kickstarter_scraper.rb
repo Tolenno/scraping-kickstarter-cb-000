718c2bb00d3f1f107519a7262d04c249578ccac2
Matt Hough <matt@hough.tv>
2017-10-14 22:52:38 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 
   projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+
+  end
 end
 
 # projects: kickstarter.css("li.project.grid_4")
