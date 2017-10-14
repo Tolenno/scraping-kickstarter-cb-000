8bd9d691c66e06830349648839148cf99a852c0c
Matt Hough <matt@hough.tv>
2017-10-14 22:24:29 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
-  kickstarter = Nokogiri::HTML("/fixtures/kickstarter.html")
+  kickstarter = Nokogiri::HTML(html)
 end
 
 create_project_hash
