7eae79eee8c89d22b0a073490854e826b7544bd1
Matt Hough <matt@hough.tv>
2017-10-14 22:56:47 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,7 +11,8 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {}
 
-    image = 
+    image = projects.css("div.project-thumbnail a img").attribute("src").value
+    projects[image.to_sym] = {}
   end
   projects
 end
