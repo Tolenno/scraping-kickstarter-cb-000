0b85bad1cf22ae38b662927f2e0b096e72864abe
Matt Hough <matt@hough.tv>
2017-10-14 22:49:57 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,5 +12,5 @@ end
 # image: kickstarter.css("div.project-thumbnail a img").attribute("src").value
 # description: kickstarter.css("p.bbcard_blurb").text
 # location: kickstarter.css("span.location-name").text
-# percent_funded: kickstarter.css("ul.project-stats li.first.funded strong").text
+# percent_funded: kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 create_project_hash
