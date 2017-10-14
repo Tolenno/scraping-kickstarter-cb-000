fe2c44522c2f7d51ca5acc66543d339a709276fd
Matt Hough <matt@hough.tv>
2017-10-14 23:03:25 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,7 +16,7 @@ def create_project_hash
 
       location: project.css("span.location-name").text
 
-      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
   projects
